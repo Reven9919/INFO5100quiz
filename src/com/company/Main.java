fd884a7ffe9ff251dff474d60de980af0797288b
Reven9919 <82441339+Reven9919@users.noreply.github.com>
2022-02-19 16:27:52 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+ublic class Person {
+    public String Name;
+    public int ID;
+    public int Age;
+
+    class Professor extends Person  {
+        @Override
+        private String Name;
+        private int ID;
+        private int Age;
+        public String giveQuiz(){
+            return String Object quiz;
+        }
+
+        Professor(int age) {
+            Age = age;
+        }
+    }
+
+    class Student extends Person{
+        @Override
+        public String takeQuiz(){
+            return quiz;
+        }
+    }
